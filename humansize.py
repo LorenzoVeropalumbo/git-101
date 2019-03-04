1181ad51efb6abca4c3f5f32d78bc12485089efe
Lorenzo Veropalumbo <Lorenzo-Veropalumbo@outlook.it>
2019-03-04 19:14:08 +0100

    add approximate

--- humansize.py
+++ humansize.py
@@ -2,7 +2,12 @@ SUFFIXES = ['KB','MB','GB','TB','PB','EB','ZB','YB']
 
 
 def approximate_size(size):
-    """ convert a file size to human-readable from."""
+    """ convert a file size to human-readable from.
+    keybord argument:
+    size -- file size in bytes
+
+    return : 
+    """
     if size < 0:
         raise ValueError('number must be non-negative')
 
